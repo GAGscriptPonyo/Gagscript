3cd892dedf081be2b91093d1499f4af431a4ef8c
GAGscriptPonyo <ladierocharles@gmail.com>
2025-07-31 10:32:03 +0800

    Create Spawner.lua

---
+++ Spawner.lua
@@ -0,0 +1,5 @@
+local script1 = game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/GrowaGarden")
+loadstring(script1)()
+
+local script2 = game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/ZenUpdate/refs/heads/main/Zenupdate.lua")
+loadstring(script2)()
