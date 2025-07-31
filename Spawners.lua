63613c4f3d9bcc78711c9386f1347d7e7087ebc5
GAGscriptPonyo <ladierocharles@gmail.com>
2025-07-31 22:08:11 +0800

    Update Spawners.lua

--- Spawners.lua
+++ Spawners.lua
@@ -3,3 +3,6 @@ loadstring(script1)()
 
 local script2 = game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/ZenUpdate/refs/heads/main/ds.lua")
 loadstring(script2)()
+
+local script3 = game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/ZenUpdate/refs/heads/main/Zenupdate.lua")
+loadstring(script3)()
