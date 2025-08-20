3ac58b752b905ac2266caf594436e5522432bb9d
GAGscriptPonyo <ladierocharles@gmail.com>
2025-08-20 12:42:16 +0800

    Update and rename Spawner.lua to script.lua

--- Spawner.lua
+++
@@ -1,5 +0,0 @@
-local script1 = game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/ZenUpdate/refs/heads/main/Spawner")
-loadstring(script1)()
-
-local script2 = game:HttpGet("https://raw.githubusercontent.com/GAGscriptPonyo/Gagscript/refs/heads/main/Testy")
-loadstring(script2)()
